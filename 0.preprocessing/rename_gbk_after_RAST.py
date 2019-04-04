f648f452480e543dbce04c6296d3ec99d0c9de26
Olga Sigalova <olga.sigalova88@gmail.com>
2019-04-04 18:31:48 +0200

    rename gbk

---
+++ 0.preprocessing/rename_gbk_after_RAST.py
@@ -0,0 +1,26 @@
+import sys
+import os
+import fnmatch
+import pandas as pd
+import numpy as np
+
+gbk_dir = "/Users/sigalova/Desktop/Chlamydia_pangenome/data/genomes_new"
+
+# original names of gbk files
+v1 = fnmatch.filter(os.listdir(gbk_dir), '*.gbk')
+# new names (D + genome counter) 
+v2 = ["D" + str(x + 1).zfill(3) for x in range(len(v1))]
+
+
+gbk_names_dict = dict(zip(v1, v2))
+
+# save names dictionary
+df = pd.DataFrame.from_dict(data = gbk_names_dict, orient='index')
+df.columns = ["New name"]
+df.to_csv(gbk_dir + "/gbk_renamed_dict.csv")
+
+# rename files
+for f in v1:
+    old_path = gbk_dir + "/" + f
+    new_path = gbk_dir + "/" + gbk_names_dict[f]
+    os.rename(old_path, new_path)
